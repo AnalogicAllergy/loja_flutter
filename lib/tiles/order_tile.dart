198dd06dd8f04a0ea44d1876b176561743a6dca6
Willian Santana <wsapcf@gmail.com>
2019-03-15 15:55:01 -0300

    Resumo do pedido

--- lib/tiles/order_tile.dart
+++ lib/tiles/order_tile.dart
@@ -1,3 +1,6 @@
+import 'dart:collection';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class OrderTile extends StatelessWidget {
@@ -7,7 +10,47 @@ class OrderTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Text(orderId),
+      margin: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+      child: Padding(
+        padding: EdgeInsets.all(8),
+        child: StreamBuilder<DocumentSnapshot>(
+          stream: Firestore.instance
+              .collection('orders')
+              .document(orderId)
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('Codigo do pedido ${snapshot.data.documentID}',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      )),
+                  SizedBox(
+                    height: 4,
+                  ),
+                  Text(_buildProductsText(snapshot.data)),
+                ],
+              );
+            }
+          },
+        ),
+      ),
     );
+  }
+
+  String _buildProductsText(DocumentSnapshot snapshot) {
+    String texto = "Descricao: \n";
+    for (LinkedHashMap t in snapshot.data["products"]) {
+      texto +=
+          "${t['quantity']} x${t['product']['title']} (R\$ ${t['product']['price'].toStringAsFixed(2)})\n";
+    }
+    texto += "Total: R\$ ${snapshot.data['totalPrice'].toStringAsFixed(2)}";
+    return texto;
   }
 }
